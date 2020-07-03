640c100c96e9ac6aa187155e2221ff17d19dd72e
Morrison Idiasirue <morrison.idiasirue@gmail.com>
2020-07-03 09:35:38 +0100

    prep to pull

--- omod/src/main/java/org/openmrs/module/limsemrops/authentication/AuthModule.java
+++ omod/src/main/java/org/openmrs/module/limsemrops/authentication/AuthModule.java
@@ -12,97 +12,97 @@ import java.time.format.DateTimeFormatter;
 import java.util.Base64;
 
 public class AuthModule {
-
-private DBManager dbManager;
-private String cachedToken = null;
-private String endPoint = null;
-private String userName = null;
-
-private boolean tokenExpired() {
-    return false; //TODO: If TG implements expiry, reimplement this
-}
-
-private String computeDefaultPassword(String username) {
-    //get the hash of the username
-    String userNameHash = AuthModuleUtils.hash(username);
-    //get the minute of the current time
-    String formattedMinute = LocalTime.now().format(DateTimeFormatter.ofPattern("mm"));
-    String concatenation = userNameHash + formattedMinute;
-    String finalHash = AuthModuleUtils.hash(concatenation);
-    return finalHash;
-}
-
-private String requestToken() throws UnirestException, SQLException {
-    //get the access token details
-    Auth a = dbManager.getAuthModuleUserNamePassword();
-    String u = a.getUsername();
-    String p = a.getPassword();
-    boolean isInitialRequest = false;
-
-    //check if we need to generate a new password or nah
-    if (p == null) {
-        //compute a default password, but set the username properly first
-        u = Utils.getFacilityDATIMId();
-        p = this.computeDefaultPassword(u);
-        isInitialRequest = true;
-    }
-
-    String requestBody = "{\"email\": \"USER\",\"password\": \"PASS\"}".replace("USER", u).replace("PASS", p);
-    HttpResponse<String> response = Unirest.post(this.endPoint).body(requestBody).asString();
-    String responseBody = response.getBody();
-    int statusCode = response.getStatus();
-    //now, if its an initial request, we should strip out the new password and use the correct method
-    if (isInitialRequest) {
-        //find the new password and extract it from the JWT, then request a new, sanitized JWT
-        String actualJWT = AuthModuleUtils.extractJWT(responseBody);
-        //base64decode the JWT so we can see what's in it.
-        String decodedJWT = new String(Base64.getMimeDecoder().decode(actualJWT));
-        //somewhere in there, we should have a key called password. Take it out.
-        String newPassword = AuthModuleUtils.extractKey("password", decodedJWT);
-        //then, persist the new password to the DB
-        Auth details = new Auth();
-        details.setUsername(Utils.getFacilityDATIMId());
-        details.setPassword(newPassword);
-        dbManager.initializeAuthModuleUserNamePassword(details); //initial setting
-        return requestToken(); //redo it, using the now-saved password
-    }
-    else {
-        if (statusCode != 200) {
-            //this is not an OK, so likely we should invalidate our current token and redo
-            Auth newAuth = new Auth();
-            newAuth.setUsername(Utils.getFacilityDATIMId());
-            newAuth.setPassword(null); //set this to null completely
-            dbManager.setAuthModuleUserNamePassword(newAuth);
-            //ideally, redo password generation here
-            return this.requestToken(); //hopefully this one should complete.
-        }
-        else {
-        //business as usual
-        return AuthModuleUtils.extractJWT(responseBody);
-        }
-    }
-    
-}
-
-public String getToken() {
-    try {
-        if (this.cachedToken == null || tokenExpired()) {
-            this.cachedToken = requestToken();
-        }
-    } catch (Exception e) {
-        return null;
-    }
-    
-    return this.cachedToken;
-}
-
-public AuthModule(String _endpoint) {
-    super();
-    this.endPoint = _endpoint;
-    this.userName = Utils.getFacilityDATIMId();
-    this.dbManager = new DBManager();
-}
-
-
-
+	
+	private DBManager dbManager;
+	
+	private String cachedToken = null;
+	
+	private String endPoint = null;
+	
+	private String userName = null;
+	
+	private boolean tokenExpired() {
+		return false; //TODO: If TG implements expiry, reimplement this
+	}
+	
+	private String computeDefaultPassword(String username) {
+		//get the hash of the username
+		String userNameHash = AuthModuleUtils.hash(username);
+		//get the minute of the current time
+		String formattedMinute = LocalTime.now().format(DateTimeFormatter.ofPattern("mm"));
+		String concatenation = userNameHash + formattedMinute;
+		String finalHash = AuthModuleUtils.hash(concatenation);
+		return finalHash;
+	}
+	
+	private String requestToken() throws UnirestException, SQLException {
+		//get the access token details
+		Auth a = dbManager.getAuthModuleUserNamePassword();
+		String u = a.getUsername();
+		String p = a.getPassword();
+		boolean isInitialRequest = false;
+		
+		//check if we need to generate a new password or nah
+		if (p == null) {
+			//compute a default password, but set the username properly first
+			u = Utils.getFacilityDATIMId();
+			p = this.computeDefaultPassword(u);
+			isInitialRequest = true;
+		}
+		
+		String requestBody = "{\"email\": \"USER\",\"password\": \"PASS\"}".replace("USER", u).replace("PASS", p);
+		HttpResponse<String> response = Unirest.post(this.endPoint).body(requestBody).asString();
+		String responseBody = response.getBody();
+		int statusCode = response.getStatus();
+		//now, if its an initial request, we should strip out the new password and use the correct method
+		if (isInitialRequest) {
+			//find the new password and extract it from the JWT, then request a new, sanitized JWT
+			String actualJWT = AuthModuleUtils.extractJWT(responseBody);
+			//base64decode the JWT so we can see what's in it.
+			String decodedJWT = new String(Base64.getMimeDecoder().decode(actualJWT));
+			//somewhere in there, we should have a key called password. Take it out.
+			String newPassword = AuthModuleUtils.extractKey("password", decodedJWT);
+			//then, persist the new password to the DB
+			Auth details = new Auth();
+			details.setUsername(Utils.getFacilityDATIMId());
+			details.setPassword(newPassword);
+			dbManager.initializeAuthModuleUserNamePassword(details); //initial setting
+			return requestToken(); //redo it, using the now-saved password
+		} else {
+			if (statusCode != 200) {
+				//this is not an OK, so likely we should invalidate our current token and redo
+				Auth newAuth = new Auth();
+				newAuth.setUsername(Utils.getFacilityDATIMId());
+				newAuth.setPassword(null); //set this to null completely
+				dbManager.setAuthModuleUserNamePassword(newAuth);
+				//ideally, redo password generation here
+				return this.requestToken(); //hopefully this one should complete.
+			} else {
+				//business as usual
+				return AuthModuleUtils.extractJWT(responseBody);
+			}
+		}
+		
+	}
+	
+	public String getToken() {
+		try {
+			if (this.cachedToken == null || tokenExpired()) {
+				this.cachedToken = requestToken();
+			}
+		}
+		catch (Exception e) {
+			return null;
+		}
+		
+		return this.cachedToken;
+	}
+	
+	public AuthModule(String _endpoint) {
+		super();
+		this.endPoint = _endpoint;
+		this.userName = Utils.getFacilityDATIMId();
+		this.dbManager = new DBManager();
+	}
+	
 }
--- omod/src/main/java/org/openmrs/module/limsemrops/authentication/AuthModuleUtils.java
+++ omod/src/main/java/org/openmrs/module/limsemrops/authentication/AuthModuleUtils.java
@@ -3,43 +3,43 @@ package org.openmrs.module.limsemrops.authentication;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 
-
 public class AuthModuleUtils {
-    public static String hash(String input) {
-        MessageDigest msgDigest;
-        try {
-            msgDigest = MessageDigest.getInstance("SHA-256");
-        }
-        catch (Exception e) {
-            return null;
-        }
-        
-        byte[] inputDigest = msgDigest.digest(input.getBytes());
-
-        BigInteger inputDigestBigInt = new BigInteger(1, inputDigest);
-
-        // Convert the input digest into hex value
-        String hashtext = inputDigestBigInt.toString(16);
-
-        //Add preceding 0's to pad the hashtext to make it 32 bit
-        while (hashtext.length() < 32) {
-            hashtext = "0" + hashtext;
-        }
-        return hashtext;
-    }
-
-    public static String extractKey(String key, String response) {
-        int startIndex = response.indexOf(String.format("%s\":\"",key) ,0);
-        int endIndex = response.indexOf("\"", startIndex + 6);
-        String actualToken = response.substring(startIndex + 6, endIndex);
-        return actualToken.trim();
-    }
-
-    public static String extractJWT(String response) {
-        return extractKey("jwt",response);
-        // int startIndex = response.indexOf("jwt\":\"",0);
-        // int endIndex = response.indexOf("\"", startIndex + 6);
-        // String actualToken = response.substring(startIndex + 6, endIndex);
-        // return actualToken.trim();
-    }
+	
+	public static String hash(String input) {
+		MessageDigest msgDigest;
+		try {
+			msgDigest = MessageDigest.getInstance("SHA-256");
+		}
+		catch (Exception e) {
+			return null;
+		}
+		
+		byte[] inputDigest = msgDigest.digest(input.getBytes());
+		
+		BigInteger inputDigestBigInt = new BigInteger(1, inputDigest);
+		
+		// Convert the input digest into hex value
+		String hashtext = inputDigestBigInt.toString(16);
+		
+		//Add preceding 0's to pad the hashtext to make it 32 bit
+		while (hashtext.length() < 32) {
+			hashtext = "0" + hashtext;
+		}
+		return hashtext;
+	}
+	
+	public static String extractKey(String key, String response) {
+		int startIndex = response.indexOf(String.format("%s\":\"", key), 0);
+		int endIndex = response.indexOf("\"", startIndex + 6);
+		String actualToken = response.substring(startIndex + 6, endIndex);
+		return actualToken.trim();
+	}
+	
+	public static String extractJWT(String response) {
+		return extractKey("jwt", response);
+		// int startIndex = response.indexOf("jwt\":\"",0);
+		// int endIndex = response.indexOf("\"", startIndex + 6);
+		// String actualToken = response.substring(startIndex + 6, endIndex);
+		// return actualToken.trim();
+	}
 }
