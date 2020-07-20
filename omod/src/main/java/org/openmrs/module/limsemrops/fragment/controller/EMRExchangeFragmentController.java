dba0f568209c4188a402cafb7f99c25209fafb39
mdada <mdada@magic.umaryland.edu>
2020-07-20 10:58:33 +0100

    minor changes

--- omod/src/main/java/org/openmrs/module/limsemrops/fragment/controller/EMRExchangeFragmentController.java
+++ omod/src/main/java/org/openmrs/module/limsemrops/fragment/controller/EMRExchangeFragmentController.java
@@ -314,8 +314,8 @@ public class EMRExchangeFragmentController {
 	// Get sample result from LIMS
 	RestTemplate restTemplate = new RestTemplate();
 	
-	@RequestMapping(/*value="/template/sample_results", */method = RequestMethod.GET)
-	public String getProductsByTemplate() {
+	@RequestMapping(/*value="/template/sample_results", */method = RequestMethod.GET, path = "returned_result")
+	public String getResultByManifest() {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		HttpEntity<String> entity = new HttpEntity<String>(headers);
